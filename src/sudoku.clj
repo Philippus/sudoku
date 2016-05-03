b6d3e475a8c782b2b3222f6d21322af447f2e343
Philippus <philippus@gmail.com>
2016-05-03 13:57:21 +0200

    - extended the solver to handle the sudoku's in the NRC

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,7 +3,54 @@
 
 (def board identity)
 
+(def nrc-board-2567
+  (board [[0 9 0 0 7 6 0 0 0]
+          [0 0 3 0 2 0 6 0 1]
+          [0 0 2 1 0 5 0 0 0]
+          [0 0 0 0 0 0 0 0 0]
+          [0 0 0 0 0 7 0 0 0]
+          [0 0 0 3 0 8 9 0 0]
+          [0 0 0 2 0 3 0 0 0]
+          [0 0 4 5 0 0 0 2 0]
+          [0 0 6 0 0 0 4 0 0]]))
+
+
+(def nrc-board-2568
+  (board [[0 0 0 0 0 9 4 0 0]
+          [0 0 0 0 5 0 0 3 0]
+          [0 4 3 2 0 0 0 0 1]
+          [0 0 0 0 0 0 0 6 0]
+          [0 0 0 0 0 0 0 1 0]
+          [0 5 0 9 0 0 0 4 8]
+          [0 0 4 1 6 0 0 0 0]
+          [2 3 0 0 7 0 0 0 0]
+          [5 0 0 0 0 0 0 0 0]]))
+
+(def nrc-board-2569
+  (board [[0 1 0 0 0 2 0 0 5]
+          [0 0 0 0 0 5 0 0 0]
+          [0 5 0 0 0 8 0 0 0]
+          [0 0 0 3 0 0 0 0 0]
+          [0 0 0 0 0 7 6 0 0]
+          [0 8 0 0 9 0 1 0 0]
+          [2 0 3 7 0 0 4 8 0]
+          [0 0 0 4 0 0 0 0 0]
+          [0 0 0 0 8 0 9 0 6]]))
+
+(def nrc-board-2570
+  (board [[1 0 0 0 0 0 0 0 0]
+          [0 0 0 0 5 0 6 0 0]
+          [0 3 0 1 0 0 0 0 0]
+          [2 0 0 0 3 9 0 0 0]
+          [0 7 0 2 0 0 0 0 0]
+          [0 0 5 0 0 0 9 0 0]
+          [0 0 0 0 0 1 0 7 4]
+          [0 0 2 0 0 0 0 3 0]
+          [8 9 0 0 7 0 0 5 0]]))
+
 (def all-values #{1 2 3 4 5 6 7 8 9})
+
+(def nrcblockcoords #{1 2 3 5 6 7})
 
 (defn value-at [board coord]
   (get-in board coord))
@@ -26,17 +73,44 @@
   (let [[row col] coord]
     [(* (int (/ row 3)) 3) (* (int (/ col 3)) 3)]))
 
+(defn five-or-one [x]
+  (if (< x 4)
+    1
+    5))
+
+(defn nrc-top-left [coord]
+    (let [[row col] coord]
+      [(five-or-one row) (five-or-one col)]))
+
 (defn block-coords [coord]
   (let [[top left] (top-left coord)]
+    (for [row [top (inc top) (inc (inc top))] col [left (inc left) (inc (inc left))]] [row col])))
+
+(defn nrc-block-coords [coord]
+  (let [[top left] (nrc-top-left coord)]
     (for [row [top (inc top) (inc (inc top))] col [left (inc left) (inc (inc left))]] [row col])))
 
 (defn block-values [board coord]
   (set (map (fn [x] (value-at board x)) (block-coords coord))))
 
+(defn in-nrc-block? [coord]
+  (let [[row col] coord]
+    (and (contains? nrcblockcoords row) (contains? nrcblockcoords col))))
+
+(defn nrc-block-values [board coord]
+  (if (in-nrc-block? coord)
+    (set (map (fn [x] (value-at board x)) (nrc-block-coords coord)))
+    #{}))
+
 (defn valid-values-for [board coord]
   (if (has-value? board coord)
     #{}
     (set/difference all-values (set/union (row-values board coord) (col-values board coord) (block-values board coord)))))
+
+(defn valid-nrc-values-for [board coord]
+  (if (has-value? board coord)
+    #{}
+    (set/difference all-values (set/union (row-values board coord) (col-values board coord) (block-values board coord) (nrc-block-values board coord)))))
 
 (defn filled? [board]
   (not (contains? (set (map (fn [x] (has-value? board x)) (coord-pairs [0 1 2 3 4 5 6 7 8]))) false)))
@@ -59,8 +133,17 @@
 (defn valid-blocks? [board]
   (every? (fn [x] (empty? (set/difference all-values x))) (blocks board)))
 
+(defn nrc-blocks [board]
+  (map (fn [coord] (nrc-block-values board coord)) (coord-pairs [1 5])))
+
+(defn valid-nrc-blocks? [board]
+  (every? (fn [x] (empty? (set/difference all-values x))) (nrc-blocks board)))
+
 (defn valid-solution? [board]
   (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
+
+(defn valid-nrc-solution? [board]
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board) (valid-nrc-blocks? board)))
 
 (defn set-value-at [board coord new-value]
   (assoc-in board coord new-value))
@@ -81,5 +164,21 @@
             solution (solve-helper (set-value-at current-board empty-location valid-value))] solution)
       ))
   )
+
+(defn solve-nrc-helper [current-board]
+  (if (filled? current-board)
+    (if (valid-nrc-solution? current-board)
+      [current-board]
+      nil)
+    (let [empty-location (find-empty-point current-board)]
+      (for [valid-value (valid-nrc-values-for current-board empty-location)
+            solution (solve-nrc-helper (set-value-at current-board empty-location valid-value))] solution)
+      ))
+  )
+
 (defn solve [board]
   (first (solve-helper board)))
+
+(defn solve-nrc [board]
+  (first (solve-nrc-helper board)))
+
